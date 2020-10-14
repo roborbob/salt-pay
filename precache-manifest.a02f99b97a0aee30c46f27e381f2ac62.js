8a0aa208a17f961a218b4fa6980697ba3f9027f9
Robert James <36674828+roborbob@users.noreply.github.com>
2020-10-14 14:53:17 +0100

    Updates

---
+++ precache-manifest.a02f99b97a0aee30c46f27e381f2ac62.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc35092b0a65a04faac1889eda1f16a",
+    "url": "//roborbob.github.io/salt-pay/index.html"
+  },
+  {
+    "revision": "e3a4b9bf84c3048dce11",
+    "url": "//roborbob.github.io/salt-pay/static/css/main.9b7503ff.chunk.css"
+  },
+  {
+    "revision": "691102e003501041b65c",
+    "url": "//roborbob.github.io/salt-pay/static/js/2.5fb0926a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//roborbob.github.io/salt-pay/static/js/2.5fb0926a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a4b9bf84c3048dce11",
+    "url": "//roborbob.github.io/salt-pay/static/js/main.96811701.chunk.js"
+  },
+  {
+    "revision": "ff799313658da08f2b70",
+    "url": "//roborbob.github.io/salt-pay/static/js/runtime-main.dabc73f8.js"
+  },
+  {
+    "revision": "344e3c2fe29d5a80db5be1cc999fcb3a",
+    "url": "//roborbob.github.io/salt-pay/static/media/footer.344e3c2f.svg"
+  },
+  {
+    "revision": "a7262e2ca1d39e1f6611f840c0187014",
+    "url": "//roborbob.github.io/salt-pay/static/media/header.a7262e2c.svg"
+  },
+  {
+    "revision": "32e29f077b3c6f75a297af430f8141c6",
+    "url": "//roborbob.github.io/salt-pay/static/media/highlights-customer-service.32e29f07.png"
+  },
+  {
+    "revision": "0f3bc1c3ffff03a9e6b19ac47f9a71fa",
+    "url": "//roborbob.github.io/salt-pay/static/media/highlights-loyalty.0f3bc1c3.png"
+  },
+  {
+    "revision": "fb088a0afeeef6c64f0d9996e30acc4c",
+    "url": "//roborbob.github.io/salt-pay/static/media/highlights-pos.fb088a0a.png"
+  },
+  {
+    "revision": "1e958e471b0c1339b7e7c87df91ffd79",
+    "url": "//roborbob.github.io/salt-pay/static/media/logo-white.1e958e47.png"
+  },
+  {
+    "revision": "627d1e33d67f9272657915e7c3b89812",
+    "url": "//roborbob.github.io/salt-pay/static/media/logo.627d1e33.svg"
+  },
+  {
+    "revision": "631a8eba8094a7b1abd91fd802d385a4",
+    "url": "//roborbob.github.io/salt-pay/static/media/pos-photos.631a8eba.png"
+  }
+]);
